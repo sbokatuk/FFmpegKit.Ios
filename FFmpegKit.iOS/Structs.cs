abf77986d3a629fd1c99c767d29cb79d8718271e
Sergei Bokatuk <s@bokatuk.com>
2022-09-20 22:22:04 +0300

    initial

---
+++ FFmpegKit.iOS/Structs.cs
@@ -0,0 +1,31 @@
+﻿using ObjCRuntime;
+using System;
+
+namespace FFmpegKit.Ios
+{ 
+    [Native]
+    public enum LogRedirectionStrategy : ulong
+    {
+        AlwaysPrintLogs,
+        PrintLogsWhenNoCallbacksDefined,
+        PrintLogsWhenGlobalCallbackNotDefined,
+        PrintLogsWhenSessionCallbackNotDefined,
+        NeverPrintLogs
+    }
+
+    [Native]
+    public enum ReturnCodeEnum : ulong
+    {
+        Success = 0,
+        Cancel = 255
+    }
+
+    [Native]
+    public enum SessionState : ulong
+    {
+        Created,
+        Running,
+        Failed,
+        Completed
+    }
+}
